f2e120a5680b893b9abcff51f8da4930f951abc6
anacondz <97612929+anacondz@users.noreply.github.com>
2022-09-10 15:54:50 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+    bodyEl = document.querySelector('body');
+    bStart = document.querySelector('[data-start]');
+    bStop = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+bStart.addEventListener("click", btnStart);
+bStop.addEventListener("click", btnStop);
+
+function btnStart() {
+    bStart.disabled = true;
+    bStop.disabled = false;
+    timerId = setInterval(() => {
+        bodyEl.style.background = getRandomHexColor()
+    }, 1000)
+}
+
+    function btnStop() {
+        bStart.disabled = false;
+        bStop.disabled = true;
+        clearInterval(timerId);
+    }

edc9a9af9e87adc4b96c4c903f8144cedfc89f96
anacondz <97612929+anacondz@users.noreply.github.com>
2022-09-11 16:52:52 +0300

    hzhzhz

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+};
+
+refs.form.addEventListener('submit', PrDefault);
+
+function PrDefault(e) {
+  e.preventDefault();
+}
+
+let evtDelay = Number(refs.delay.value);
+let evtStep = Number(refs.step.value);
+let evtAmount = Number(refs.amount.value);
+
+for (i = 1; i <= evtAmount; i += 1) {
+  delay += step;
+  createPromise(i, delay)
+}
+function createPromise(i, delay) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+    if (shouldResolve) {
+      resolve();
+    } else {
+      reject();
+    }
+  }, delay);
+});
+
+  createPromise(i, delay)
+  .then(({ i, delay }) => {
+    console.log(`✅ Fulfilled promise ${i} in ${delay}ms`);
+  })
+  .catch(({ i, delay }) => {
+    console.log(`❌ Rejected promise ${i} in ${delay}ms`);
+  });
 }
